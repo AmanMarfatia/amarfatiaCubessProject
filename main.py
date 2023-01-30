b83c7a9888c931120c6023e7e4a5916c3d6eb81a
amanmarfatia <amarfatia@student.bridgew.edu>
2023-01-29 22:53:52 -0500

    New commit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-#import requests
-
-#response = requests.get('https://comp490project.wufoo.com/forms/2023-ultimate-frisbee-tournament')
-#form = response.json()
-#print(response.text)
-
 import urllib2
 import json
 
-base_url = 'https://comp490project.wufoo.com/forms/2023-ultimate-frisbee-tournament/api/v3/'
+base_url = 'https://comp490project.wufoo.com/api/v3/'
 username = 'G0QW-6YXP-H7LX-TN6S'
 password = 'comp490'
 
@@ -19,7 +13,5 @@ urllib2.install_opener(opener)
 
 response = urllib2.urlopen(base_url+'forms.json')
 data = json.load(response)
-print(json.dumps(data, indent=4, sort_keys=True))
-
-
+print json.dumps(data, indent=4, sort_keys=True)
 
