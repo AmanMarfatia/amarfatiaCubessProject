f3dac38012cf8b9d2d48f10e109a27955c8262f9
amanmarfatia <amarfatia@student.bridgew.edu>
2023-01-30 22:18:50 -0500

    New commit

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from secrets import wufoo_key
 from requests.auth import HTTPBasicAuth
 
 def get_wufoo_data() -> dict:
-    url = "https://comp490project.wufoo.com/api/code/1/json"
-
+    url = "https://comp490project.wufoo.com/api/v3/forms/2023-ultimate-frisbee-tournament/entries/json"
+   # http:/comp490project.wufoo.com/api/v3/forms/{identifier}/comments/count.json
+   # https://comp490project.wufoo.com/api/v3/forms/2023-ultimate-frisbee-tournament/entries/json
 
     response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
     if response.status_code != 200:  # if we don't get an ok response we have trouble
@@ -16,6 +17,9 @@ def get_wufoo_data() -> dict:
     jsonresponse = response.json()
     return jsonresponse  # json response will be either a dictionary or a list of dictionaries
     # each dictionary represents a json object
+def main():
+    alldata = get_wufoo_data()
+    print(alldata)
 
-
-
+if __name__ =="__main__":
+    main()
