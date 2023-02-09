0b7c68e04408d57c6e8572232b2dd942358147f4
amanmarfatia <amarfatia@student.bridgew.edu>
2023-02-09 00:08:19 -0500

    New commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
+from sqlite3 import Cursor
+
 import requests
 from secrets import wufoo_key
 from requests.auth import HTTPBasicAuth
 import json
 import sqlite3
+
+data1 = {'EntryId': '1', 'Field2': 'Dr.', 'Field8': '', 'Field9': '', 'Field10': '', 'Field11': '', 'Field12': '',
+         'Field216': '', 'Field14': '', 'Field15': '', 'Field16': '', 'Field17': '', 'Field18': '', 'Field115': '',
+         'Field116': '', 'Field117': '', 'Field118': '', 'DateCreated': '2023-01-27 19:43:55', 'CreatedBy': 'public',
+         'DateUpdated': '', 'UpdatedBy': None}
 
 
 def get_wufoo_data() -> dict:
@@ -24,17 +31,52 @@ def file_save(data):
         file.write(json.dumps(data))
 
 
-conn = sqlite3.connect('cube_database.sqlite')
-cursor = conn.cursor()
+# make this a function where where it takes data as a parameter
+# conn = sqlite3.connect('cube_database.sqlite')
+print("Database conection")
+
+
+def data_base_insert():
+    conn = sqlite3.connect('/Users/amanmarfatia/PycharmProjects/amarfatiaCubessProject/cube_database.sqlite3')
+    cursor: Cursor = conn.cursor()
+    cursor.execute('''INSERT INTO 'cube_data' ( EntryId, Prefix, First, Last, Logo, TeamName, Email, JerseyColor, PhoneNumber, Coach, HeadCoach, Waterboys, Doctor, EighteenThruTwentyFive, TwentyfiveThruThirty, ThirtyThruForty, FortyThruFifty),
+        VALUES(
+        '1',
+        'Dr',
+        '',
+        'Null',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+        ''')
+
+    conn.commit()
+    conn.close()
+
+
 print("Open database successfully")
 
-conn.commit()
-conn.close()
 
 def main():
     alldata = get_wufoo_data()
-    file_save(alldata)
-    print(alldata)
+    # file_save(alldata)
+    # print(alldata)
+    array_of_entries = alldata["Entries"]
+
+    for arr in array_of_entries:
+        print(arr)
+
+    data_base_insert()
 
 
 if __name__ == "__main__":
